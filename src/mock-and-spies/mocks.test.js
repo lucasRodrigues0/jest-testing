1be8e9470ce848e4e1f2dfdaf321791c396aebb5
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-11-12 14:52:09 -0300

    mock e spy

---
+++ src/mock-and-spies/mocks.test.js
@@ -0,0 +1,18 @@
+test('basic mock function', () => {
+    const mock = jest.fn(x => 42 + x);
+    expect(mock(1)).toBe(43);
+    expect(mock).toHaveBeenCalledWith(1);
+})
+
+test('spying on a method of an object', () => {
+    const video = {
+        play: () => {
+            return true;
+        }
+    }
+
+    const spy = jest.spyOn(video, 'play');
+    video.play();
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+})
